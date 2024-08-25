f37524b0bebebf0b12c02da9f8b9ce1099979b43
Boluchel <boluchel123@gmail.com>
2024-08-25 11:39:38 +0100

    Update ERC20Token.sol

--- contracts/ERC20Token.sol
+++ contracts/ERC20Token.sol
@@ -1,61 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import "./IERC20Token.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is IERC20Token {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-    address owner;
-
-    mapping(address => uint256) balance;
-
-    event Burn(address indexed from, uint256 value);
-    event Mint(address indexed to, uint256 value);
-
-
-
-    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _initialSupply) {
-        name = _name;
-        symbol = _symbol;
-        decimals = _decimals;
-        totalSupply = _initialSupply * 10 ** _decimals;
-        balance[msg.sender] = totalSupply;
-        owner = msg.sender;
+contract Tokens is ERC20, Ownable {
+    constructor(
+        uint256 initialSupply
+    ) ERC20("Bolu", "BO") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
     }
 
-    function onlyOwner() view private {
-        require(msg.sender == owner, "Only Onwer can perform this transaction");
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
     }
 
-    function transfer(address _to, uint256 _value) external returns (bool success) {
-        require( msg.sender != address(0), "Address zero cannot make a transfer");
-        require( balance[msg.sender] > _value , "You don't have enough balance");
-        balance[msg.sender] = balance[msg.sender] - _value;
-        balance[_to] = balance[_to] + _value;
-        emit Transfer(msg.sender, _to, _value);
-        return true;
+    function burn(address from, uint256 amount) external {
+        _burn(from, amount);
     }
-
-    function mint(address _to, uint256 _value) external {
-        onlyOwner();
-        require(_to != address(0), "Invalid address");
-        require(totalSupply + _value >= totalSupply, "Overflow error");
-
-        balance[_to] =  balance[_to] + _value;
-        totalSupply = totalSupply + _value;
-
-        emit Mint(_to, _value);
-    }
-
-    function burn(uint256 _value) public {
-        require(balance[msg.sender] >= _value, "Insufficient balance");
-
-        balance[msg.sender] = balance[msg.sender] - _value;
-        totalSupply = totalSupply - _value;
-
-        emit Burn(msg.sender, _value);
+    function transfer(address _from, address _to, uint256 _amount)external {
+        _transfer(_from, _to, _amount);
     }
 }
